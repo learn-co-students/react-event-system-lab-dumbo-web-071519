ba56915809a3a56c88589a5bc39f2f53fd0d1d8f
Alessandra Vertrees <avertrees447@gmail.com>
2019-09-17 10:55:44 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,29 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react";
+
+// ## Eyes on the ball
+
+// Let's say you're in the club with your buddy.The music's blaring, lights are
+// flashing...It's so hard to get his attention! Your job is to create a component
+// that registers whenever he focuses on you, _and_ when his eyes are drifting off.
+
+// 1. In the`components/EyesOnMe.js` file, create a`EyesOnMe` React component.
+// 2. In that component, render a`button`.
+// 3. On that`button`, add event handlers that listens for the`focus` and`blur` events.
+// 4. When the`focus` event fires, use`console.log` to print out the text`'Good!'`.
+// 5. When the`blur` event fires, use`console.log` to print out the text`'Hey! Eyes on me!'`.
+
+export default class EyesOnMe extends Component {
+    handleFocus(event){
+        console.log("Good!")
+    }
+
+    handleBlur(event){
+        console.log("Hey! Eyes on me!")
+    }
+    render() {
+        return (
+            <button onFocus={this.handleFocus} onBlur={this.handleBlur}>Button</button>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,27 @@
 // Code Keypad Component Here
+import React, {Component} from "react";
+
+// ## Keypad
+// ![Keypad](https://media.giphy.com/media/3o6MbdTboTFWOUsLkc/giphy.gif)
+
+//     Mr.Burns has requested us to build a new keypad component for the nuclear
+// plant, since the last one was way too complicated for his employees to use.
+//     We'll keep things super simple instead, and use an `<input type="password" />`
+// field to capture input.Here's how to complete the exercise:
+
+// 1. In the`components/Keypad.js` file, create a`Keypad` React component.
+// 2. In that component, render an`input` with the right type.
+// 3. On that`input`, add an event handler that listens for the`keyUp` event.
+// 4. When that event fires, use`console.log` to print out the text`'Entering password...'`.
+
+export default class Keypad extends Component {
+    handleKeyUp(event){
+        console.log("Entering password...")
+    }
+    render(){
+        return(
+            <input type="password" onKeyUp={this.handleKeyUp}></input>
+        )
+
+    }
+}
