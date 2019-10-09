481c5663c9b58ee879c7c7fa3b277d6307901148
github username <github email address>
2019-10-09 17:10:45 -0400

    event handler functions should not be called but referenced and must be preceded with this if in a class component

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+    state = { }
+
+    eventHandlerOnFocus = () => (console.log("Good!"))
+    eventHandlerOnBlur = () => (console.log("Hey! Eyes on me!"))
+
+    render() { 
+        return ( 
+            <button onFocus={ this.eventHandlerOnFocus } onBlur={ this.eventHandlerOnBlur }>
+                Eyes on me please!
+            </button> )
+    }
+}
+ 
